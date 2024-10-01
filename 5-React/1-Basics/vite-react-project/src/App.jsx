c4a6b5371fec9b57f673d26785969650efb96721
Tahir Khan <tahirtrk42@gmail.com>
2024-10-01 14:01:55 +0530

    deep dive into internal working of react library

--- 5-React/1-Basics/vite-react-project/src/App.jsx
+++ 5-React/1-Basics/vite-react-project/src/App.jsx
@@ -2,10 +2,11 @@ import Learn from "./Learning"
 import Test from "./Test"
 
 function App() {
-
+  let value = "Khan"
+  // Inserting variables inside an HTML tag.
   return (
     <>
-      <h1>Started Learning React from chai aur code | Tahir</h1>
+      <h1>Started Learning React from chai aur code | Tahir {value} </h1>
       <Learn />
       <Test />
     </>
--- 5-React/1-Basics/vite-react-project/src/main.jsx
+++ 5-React/1-Basics/vite-react-project/src/main.jsx
@@ -1,9 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+function MyApp() {
+  return (
+    <h3>Learning react on Day-2</h3>
+  )
+}
+
+// This is our custom object, which is not recognized by react library, This will not work while rendering
+
+// const element = {
+//   type: "a",
+//   props: {
+//     href: "https://facebook.com",
+//     target: "_blank",
+//   },
+//   children: "Click to visit Facebook",
+// };
+
+const anotherEle = (
+  <a href="https://netflix.com" target='_blank'>Visit to Netflix website</a>
+)
+
+const name = "Tahir Khan"
+
+const reactEle = React.createElement(
+  'h1',
+  {title: "I am heading"},
+  "Testing with react element ",
+  name
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    // <App/>
+    // MyApp()
+    // element 
+    // <MyApp />
+    // anotherEle
+    reactEle
 )
