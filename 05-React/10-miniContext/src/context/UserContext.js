3e4b32bd4fe794cc1f56538eb029e82b76f87580
Tahir Khan <tahirtrk42@gmail.com>
2024-10-16 11:51:29 +0530

    Revision context API

--- 05-React/10-miniContext/src/context/UserContext.js
+++ 05-React/10-miniContext/src/context/UserContext.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// This creates a UserContext object, which can hold data that you want to share across components.
 const UserContext = React.createContext();
 
 export default UserContext;
