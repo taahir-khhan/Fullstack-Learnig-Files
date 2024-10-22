9abece760d0c5206cd92bc8b6f315b66ca7d9540
Tahir Khan <tahirtrk42@gmail.com>
2024-10-22 09:08:19 +0530

    storing all react projects in a single vite project

---
+++ 05-React/reactPracticeProjects/src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-5xl text-white text-center">All Practice Projects</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ 05-React/reactPracticeProjects/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as BackgroundChanger } from "./backgroundChanger/BackgroundChanger.jsx";
+export { default as CounterApp } from "./counterApp/CounterApp.jsx";
+export { default as Header } from "./Header/Header.jsx";
+export { default as Home } from "./Home/Home.jsx";
+export { default as RandomPassword } from "./randomPasswordGenerator/RandomPassword.jsx";
