10e44a039f6ce2118c65403f7965234dd02fa4c9
Tahir Khan <tahirtrk42@gmail.com>
2024-11-06 17:04:56 +0530

    some code changes

--- 05-React/14-AppwriteBlogApp/src/components/PostCard.jsx
+++ 05-React/14-AppwriteBlogApp/src/components/PostCard.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import appwriteService from "../appwrite/config";
 
 function PostCard({ $id, title, featuredImage }) {
+  // console.log(appwriteService.getFilePreview(featuredImage));
   return (
     <Link to={`/post/${$id}`}>
       <div className="w-full bg-gray-100 rounded-xl p-4">
