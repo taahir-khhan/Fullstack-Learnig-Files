12d6a730a292de45f9fd5fc0c31a4121ef3482d5
Tahir Khan <tahirtrk42@gmail.com>
2025-01-04 17:53:38 +0530

    making a robust bakend for my vidtube application and connected mongodb with my application

---
+++ 11-VidTube/src/app.js
@@ -0,0 +1,19 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+// cors middleware, means only serve to allowed application to access this application data
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// common middleware
+app.use(express.json({ limit: "16kb" }));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+export { app };
---
+++ 11-VidTube/src/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is listening at port ${PORT} & MongoDB is connected`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error");
+  });
