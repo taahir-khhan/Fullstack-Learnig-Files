94d977083452bbba452df050928f1a76c5a6d867
Tahir Khan <tahirtrk42@gmail.com>
2025-01-06 20:59:06 +0530

    little bit learning from udemy today, not so much satisfied still it's better than nothing

--- 11-VidTube/src/app.js
+++ 11-VidTube/src/app.js
@@ -16,4 +16,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
+// import routes
+import healthcheckRoutes from "./routes/healthcheck.routes.js";
+
+
 export { app };
---
+++ 11-VidTube/src/routes/healthcheck.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { healthcheck } from "../controllers/healthcheck.controllers";
+
+const router = Router();
+router.route("/").get(healthcheck);
+
+export default router;
