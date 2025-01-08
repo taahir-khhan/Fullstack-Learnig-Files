c4e49ecee8b365997e4b29a385cf787d0e9fcc61
Tahir Khan <tahirtrk42@gmail.com>
2025-01-08 15:39:44 +0530

    building healthcheck routes, building models in mongoDB with aggregation pipeline and hooks and methods in mongoose with JWT. quite fun today learning

--- 11-VidTube/src/app.js
+++ 11-VidTube/src/app.js
@@ -1,5 +1,7 @@
 import cors from "cors";
 import express from "express";
+import morgan from "morgan";
+import logger from "./logger.js";
 
 const app = express();
 
@@ -16,8 +18,28 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-// import routes
-import healthcheckRoutes from "./routes/healthcheck.routes.js";
+const morganFormat = ":method :url :status :response-time ms";
 
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+// import routes
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+
+// routes
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export { app };
---
+++ 11-VidTube/src/models/user.models.js
@@ -0,0 +1,89 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary URL
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary URL
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  // Simple thing here, only encrypt the password when it is added or updated, only then you will re-encrypt the password otherwise not
+  if (!this.modified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+
+  next();
+});
+
+// This is for when user types the real password and stored password in DB is encrypted, but it basically decrypt the password and compare with stored password
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  // short lived access token
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
